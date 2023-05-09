57a66e6afbd2e1f5f9c9badbea410109431d7038
LF0311 <72747869+LF0311@users.noreply.github.com>
2023-05-09 17:53:19 +0800

    Update read file.py

--- read file.py
+++ read file.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import streamlit as st
 
-k = pd.read_csv('./sensor_files/sensor_1_gps.txt', sep=',', header=None)
-print(k)
+txt_name_time_df = pd.read_csv('./sensor_files/sensor_1_gps.txt', sep=',', header=None)
+st.dataframe(txt_name_time_df, height=650)
+
