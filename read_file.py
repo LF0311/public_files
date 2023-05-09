e51b880c455cb3e1372e2f85c695042a0a07e4eb
LF0311 <72747869+LF0311@users.noreply.github.com>
2023-05-09 18:02:22 +0800

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 
 txt_name_time_df = pd.read_csv('./sensor_files/sensor_1_gps.txt', sep=',', header=None)
-directory = './sensor_files'
+directory = './sensor_files/'
 a = os.listdir(directory)
 st.dataframe(txt_name_time_df, height=650)
 st.markdown(a)
