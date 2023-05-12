a66e991705ebaf86052fef33de34681769b6de4c
LF0311 <72747869+LF0311@users.noreply.github.com>
2023-05-12 11:29:28 +0800

    Update Multipages_0_0_3_test.py

--- Multipages_0_0_3_test.py
+++ Multipages_0_0_3_test.py
@@ -1083,7 +1083,7 @@ def app1():
         )
         # st.markdown(trans_data)
         json_data = json.dumps(trans_data)
-        st.markdown(json_data)
+        # st.markdown(json_data)
         # 发送POST请求
         url = 'http://51.51.51.15:18000/resourceManagerWLW/sendTargetInfo'
         response = requests.post(url, json=json_data)
